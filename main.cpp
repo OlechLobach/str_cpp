4fd265b8377d5a99b7be0ed7dc9464e343bc9508
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-12 19:18:56 +0200

    initiall commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,49 @@
-#include<iostream>
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+inline uint64_t strLen(const char* str)
+{
+    uint64_t len = 0;
+    for (size_t i = 0; str[i] != '\0'; i++)
+    {
+        len++;
+    }
+    return len;
+}
+
+void countWords(const char* str)
+{
+    uint64_t wordCount = 0;
+    bool inWord = false;
+
+    for (size_t i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+        {
+            inWord = false;
+        }
+        else if (!inWord)
+        {
+            inWord = true;
+            wordCount++;
+        }
+    }
+
+  
+    cout << "Word count: " << wordCount << endl;
+}
 
 int main()
 {
-	return 0;
+    const char pattern[] = ",.!?#:;() {} [] @* \'\"_-";
+    const uint64_t size = 1000;
+    char str[size];
+    cout << "Enter string: ";
+    gets_s(str, size);
+
+    
+    countWords(str);
+
+    return 0;
 }
