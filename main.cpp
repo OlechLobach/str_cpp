54feb2dcf59d475f93c82d97e70bf3d646c2f32e
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-12 19:14:12 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
-#include<iostream>
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+inline uint64_t strLen(const char* str)
+{
+    uint64_t len = 0;
+    for (size_t i = 0; str[i] != '\0'; i++)
+    {
+        len++;
+    }
+    return len;
+}
+
+void replaceSpacesWithTabs(char* str)
+{
+    for (size_t i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == ' ')
+        {
+            str[i] = '\t';
+        }
+    }
+}
 
 int main()
 {
-	return 0;
+    const char pattern[] = ",.!?#:;() {} [] @* \'\"_-";
+    const uint64_t size = 1000;
+    char str[size];
+    cout << "Enter string: ";
+    gets_s(str, size);
+
+   
+    replaceSpacesWithTabs(str);
+
+  
+    cout << "Modified string: " << str << endl;
+
+    return 0;
 }
