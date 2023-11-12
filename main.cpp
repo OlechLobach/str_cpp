a9b905a7dee03de2ba25478a31a7e562e3ee303e
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-12 19:15:55 +0200

    initiall commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,51 @@
-#include<iostream>
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+inline uint64_t strLen(const char* str)
+{
+    uint64_t len = 0;
+    for (size_t i = 0; str[i] != '\0'; i++)
+    {
+        len++;
+    }
+    return len;
+}
+
+void countCharacters(const char* str)
+{
+    uint64_t letters = 0, digits = 0, otherChars = 0;
+
+    for (size_t i = 0; str[i] != '\0'; i++)
+    {
+        if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+        {
+            letters++;
+        }
+        else if (str[i] >= '0' && str[i] <= '9')
+        {
+            digits++;
+        }
+        else
+        {
+            otherChars++;
+        }
+    }
+
+    
+    cout << "Letters: " << letters << ", Digits: " << digits << ", Other characters: " << otherChars << endl;
+}
 
 int main()
 {
-	return 0;
+    const char pattern[] = ",.!?#:;() {} [] @* \'\"_-";
+    const uint64_t size = 1000;
+    char str[size];
+    cout << "Enter string: ";
+    gets_s(str, size);
+
+  
+    countCharacters(str);
+
+    return 0;
 }
